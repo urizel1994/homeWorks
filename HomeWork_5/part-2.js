16d7897189c946edf9ad5aae25a067feea27b94c
AleksanderPotapov <42351436+urizel1994@users.noreply.github.com>
2018-10-08 10:37:26 +0300

    внес поправки

--- HomeWork_5/part-2.js
+++ HomeWork_5/part-2.js
@@ -1,4 +1,4 @@
-/*Задание 1 */
+﻿/*Задание 1 */
 
 function forEach(array, fn) {
 	for (var i = 0; i < array.length; i++) {
@@ -16,13 +16,12 @@ function map(array, fn) {
   	return result;
 };
 
-var a = [1,2,4,10,20];
 
 /*Задание 3 */
 
 function reduce(array, fn, initial) {
 	result = initial;
-  	for (i = 0; i < array.length; i++) {
+  	for (var i = 0; i < array.length; i++) {
    		result = fn(result, array[i], i, array);
   	}
   	return result;
@@ -42,17 +41,18 @@ function upperProps(obj) {
 
 function slice(array, from = 0, to = array.length) { 
 	var result = []; 
+	if(to > array.length){
+		to = array.length;
+	}
 	if(from < 0){
 		from = array.length - (from*(-1));
 	}
 	if(to < 0){
 		to = array.length - (to*(-1));
 	}
-	for(i = from; i < to; i++){ 
+	for(var i = from; i < to; i++){ 
 		result.push(array[i]); 
 	} 
 	return result; 
 }
-
-
 
