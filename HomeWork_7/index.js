fa3257cdb6df844c126c4635000c00dd4e213818
AleksanderPotapov <42351436+urizel1994@users.noreply.github.com>
2018-10-09 13:13:44 +0300

    Update index.js

--- HomeWork_7/index.js
+++ HomeWork_7/index.js
@@ -1,16 +1,16 @@
-// задание 1
+// Р·Р°РґР°РЅРёРµ 1
 
 function addListener(eventName, target, fn) {
 	target.addEventListener(eventName, fn);	
 }
 
-// задание 2
+// Р·Р°РґР°РЅРёРµ 2
 
 function removeListener(eventName, target, fn) {
 	target.removeEventListener(eventName, fn);	
 }
 
-// задание 3
+// Р·Р°РґР°РЅРёРµ 3
 
 function skipDefault(eventName, target) {
 	target.addEventListener(eventName, function(event){
@@ -18,14 +18,14 @@ function skipDefault(eventName, target) {
 	});
 }
 
-// задание 4
+// Р·Р°РґР°РЅРёРµ 4
 
 function emulateClick(target) {
 	var click = new Event("click");
   	target.dispatchEvent(click);
 }
 
-// задание 5
+// Р·Р°РґР°РЅРёРµ 5
 
 function delegate(target, fn) {
 	target.addEventListener("click", function(event){
@@ -35,7 +35,7 @@ function delegate(target, fn) {
 	});
 }
 
-// задание 6
+// Р·Р°РґР°РЅРёРµ 6
 
 function once(target, fn) {
 
